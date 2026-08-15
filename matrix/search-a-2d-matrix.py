028c14ed949dab31afbbe3354d18a1166db1b988
Bao Luu <87928752+Giabao252@users.noreply.github.com>
2026-08-15 15:23:00 -0400

    LC-search-a-2d-matrix [Runtime: N/A; Memory: N/A]

--- matrix/search-a-2d-matrix.py
+++ matrix/search-a-2d-matrix.py
@@ -1,9 +1,9 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        left = 0
-        right = m*n - 1
         m = len(matrix)
         n = len(matrix[0])
+        left = 0
+        right = m*n - 1
 
         while left <= right:
             mid = (left + right) // 2
