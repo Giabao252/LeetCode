8f73a7742ceddc4c1b7a9ea560f7b1c5654b13cc
Bao Luu <87928752+Giabao252@users.noreply.github.com>
2026-08-15 14:43:19 -0400

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- union-find/longest-consecutive-sequence.py
+++ union-find/longest-consecutive-sequence.py
@@ -3,9 +3,9 @@ class Solution:
         numSet = set(nums)
         longest = 0
         
-        for i in numSet:
+        for i in numSet: #iterate through set is faster than through list
             if (i - 1) not in numSet: 
-                length = i + 1
+                length = i + 1 #start of sequence identified
                 while length in numSet:
                     length += 1
                 longest = max(longest, length - i)        
