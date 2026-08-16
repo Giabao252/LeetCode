fa04ed3040db29dd3b69b0886de246580b04b3f1
Bao Luu <87928752+Giabao252@users.noreply.github.com>
2026-08-16 14:32:06 -0400

    LC-remove-element [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/remove-element.py
@@ -0,0 +1,25 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        i = 0
+        j = len(nums) - 1
+
+        while i <= j:
+            if nums[i] != val and nums[j] != val:
+                k += 1
+                i += 1
+            elif nums[i] != val and nums[j] == val: 
+                k += 1
+                j -= 1
+                i += 1
+            elif nums[i] == val and nums[j] != val:
+                temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+                k += 1
+                i += 1
+                j -= 1
+            else:
+                j -= 1
+            
+        return k
