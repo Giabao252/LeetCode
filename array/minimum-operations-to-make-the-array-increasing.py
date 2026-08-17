695906d600accb394c192fde01dcd5e3c907a5cf
Bao Luu <87928752+Giabao252@users.noreply.github.com>
2026-08-17 14:51:31 -0400

    LC-minimum-operations-to-make-the-array-increasing [Runtime: N/A; Memory: N/A]

--- array/minimum-operations-to-make-the-array-increasing.py
+++ array/minimum-operations-to-make-the-array-increasing.py
@@ -2,15 +2,10 @@ class Solution:
     def minOperations(self, nums: List[int]) -> int:
         ops = 0
 
-        for i in range(len(nums)-1):
-            j = i + 1
-            if nums[i] < nums[j]:
-                continue
-            elif nums[i] == nums[j]:
-                nums[j] += 1
-                ops += 1
-            else: 
-                while nums[i] >= nums[j]:
-                    nums[j] += 1
-                    ops += 1
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i - 1]:
+                needed = nums[i - 1] + 1 - nums[i]
+                ops += needed
+                nums[i] += needed 
+
         return ops
