8db44bfa1143036f4850c15aa2c5db2730e6cfbf
Bao Luu <87928752+Giabao252@users.noreply.github.com>
2026-08-17 14:50:34 -0400

    LC-minimum-operations-to-make-the-array-increasing [Runtime: N/A; Memory: N/A]

--- array/minimum-operations-to-make-the-array-increasing.py
+++ array/minimum-operations-to-make-the-array-increasing.py
@@ -3,14 +3,14 @@ class Solution:
         ops = 0
 
         for i in range(len(nums)-1):
-            for j in range (i+1, len(nums)):
-                if nums[i] < nums[j]:
-                    continue
-                elif nums[i] == nums[j]:
+            j = i + 1
+            if nums[i] < nums[j]:
+                continue
+            elif nums[i] == nums[j]:
+                nums[j] += 1
+                ops += 1
+            else: 
+                while nums[i] >= nums[j]:
                     nums[j] += 1
                     ops += 1
-                else: 
-                    while nums[i] >= nums[j]:
-                        nums[j] += 1
-                        ops += 1
         return ops
