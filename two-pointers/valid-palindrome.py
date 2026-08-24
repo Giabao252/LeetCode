d68eceb75dbf2956324c2213e80def1073168983
Bao Luu <87928752+Giabao252@users.noreply.github.com>
2026-08-24 13:41:37 -0400

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/valid-palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean_s = [c.lower() for c in s if c.isalnum()]
+        i = 0
+        j = len(clean_s) - 1
+
+        if clean_s == []:
+            return True
+
+        while i != j:
+            if clean_s[i] != clean_s[j]:
+                return False
+            i += 1
+            j -= 1
+            
+
+        return True
+
+
+        
